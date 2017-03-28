36f6011fa5c6ccf36fc3c79298e8ce70b2c81c5a
Unknown <bailli_k@etna-alternance.net>
2017-03-28 10:41:20 +0200

    add some rules

--- tokenization.php
+++ tokenization.php
@@ -1,38 +1,42 @@
 <?php
 
 $rules = [
-  [ 'if', 'IF' ],
-  [ '\(', 'LEFT_PAREN' ],
-  [ '\)', 'RIGHT_PAREN' ],
-  [ '\{', 'LEFT_BRACE' ],
-  [ '\}', 'RIGHT_BRACE' ],
-  [ '\d+', 'INTEGER' ],
+    [ 'if',   'IF' ],
+    [ '\(',   'LEFT_PAREN' ],
+    [ '\)',   'RIGHT_PAREN' ],
+    [ '\{',   'LEFT_BRACE' ],
+    [ '\}',   'RIGHT_BRACE' ],
+    [ '\d+',  'INTEGER' ],
+    [ '\;',   'END'],
+    [ '\"',   'DOUBLE_QUOTE' ],
+    [ 'else', 'ELSE' ],
+    [ 'print', 'PRINT' ],
 ];
 
 $code = ' if (1) {}';
 $result = [];
 
 while ($code) {
-  $code = ltrim($code);
-  $valid = false;
-  foreach ($rules as $rule) {
-    $pattern = "/^" . $rule[0] . "/";
-    $type = $rule[1];
+    $code = ltrim($code);
+    $valid = false;
+    foreach ($rules as $rule) {
+        $pattern = "/^" . $rule[0] . "/";
+        $type = $rule[1];
 
     if (preg_match($pattern, $code, $capture)) {
-      $result[] = [
-        'type' => $type,
-        'value' => $capture[0],
-      ];
-      $valid = true;
-      $code = substr($code, strlen($capture[0]));
-      break;
+        $result[] = [
+            'type' => $type,
+            'value' => $capture[0],
+        ];
+        $valid = true;
+        $code = substr($code, strlen($capture[0]));
+        break;
+        }
     }
-  }
-  if (!valid) {
-    var_dump($result);
-    exit('Unable to find a rule: ' . $code);
-  }
+    if (!valid) {
+        var_dump($result);
+        exit('Unable to find a rule: ' . $code);
+    }
 }
 var_dump($result);
 ?>
